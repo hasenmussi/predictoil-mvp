233d0f40b6522d98da546438ed0089e349b9588d
hasenmussi <66254092+hasenmussi@users.noreply.github.com>
2025-07-26 16:01:59 -0300

    sensor_simulator.py

--- sensor_simulator.py
+++ sensor_simulator.py
@@ -6,7 +6,16 @@ from datetime import datetime
 
 # Autenticación con Google Sheets
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credenciales.json", scope)
+import os
+import json
+from oauth2client.service_account import ServiceAccountCredentials
+
+# Leer credenciales desde el secreto
+creds_json = os.getenv("GOOGLE_CREDENTIALS")
+creds_dict = json.loads(creds_json)
+
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 
 # Abrir la hoja de cálculo y seleccionar la primera hoja
